14e3469159d1d6170b6e2bcaba79dc49fd038684
tyfl056 <mannatant@gmail.com>
2021-07-28 13:39:02 +0530

    fixed model.sh

--- model.sh
+++ model.sh
@@ -1,4 +1,4 @@
-wget https://www.dropbox.com/s/f1h1ko0d5ce9i40/model_v2.zip?dl=0
+wget https://www.dropbox.com/s/fk1h3lxckad356k/model_v2.zip?dl=0
 mv 'model_v2.zip?dl=0' model.zip
 unzip model.zip
 rm model.zip
